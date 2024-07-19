4b10bfdf425157bb9c59d18518f35ce176102768
Sebastian Sandqvist <seb@sparque.me>
2024-07-18 18:53:39 -0700

    set

--- src/menu.ts
+++ src/menu.ts
@@ -1,21 +1,19 @@
-import { updateAndDraw as pongUpdateAndDraw } from "./pong";
-import { updateAndDraw as bulletHellUpdateAndDraw } from "./bullet-hell";
+import { updateAndDraw as pongUpdateAndDraw } from './pong';
+import { updateAndDraw as bulletHellUpdateAndDraw } from './bullet-hell';
+import { updateAndDraw as setUpdateAndDraw } from './set';
 
-const games = {
-  pong: pongUpdateAndDraw,
-  "bullet hell": bulletHellUpdateAndDraw,
-};
-const gameNames = Object.keys(games);
+const games = [
+  ['pong', pongUpdateAndDraw],
+  ['bullet hell', bulletHellUpdateAndDraw],
+  ['set', setUpdateAndDraw],
+] as const;
 
-let game = null as null | keyof typeof games;
+// let game = null as null | (typeof games)[number];
+let game = games[2] as null | (typeof games)[number];
 let index = 0;
 let animatedIndex = 0;
 
-export function updateAndDraw(
-  canvas: HTMLCanvasElement,
-  ctx: CanvasRenderingContext2D,
-  dt: number,
-) {
+export function updateAndDraw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, dt: number) {
   // UPDATE
   /////////////
   if (!game) {
@@ -41,10 +39,10 @@ export function updateAndDraw(
 
     const fontSize = 30;
     // draw the current game in the middle
-    ctx.fillStyle = "white";
+    ctx.fillStyle = 'white';
     ctx.font = `${fontSize}px Arial`;
-    ctx.textAlign = "center";
-    const gameNames = Object.keys(games);
+    ctx.textAlign = 'center';
+    const gameNames = games.map(([name]) => name);
     const canvasRect = canvas.getBoundingClientRect();
     const numberToDrawAboveAndBelow = 4;
     const spacing = 10;
@@ -52,21 +50,12 @@ export function updateAndDraw(
     const totalToDraw = numberToDrawAboveAndBelow * 2 + 1;
     const center = Math.floor(totalToDraw / 2);
     for (let i = 0; i < totalToDraw; i++) {
-      const gameIndex = modulusThatHandlesNegatives(
-        index + i - center,
-        gameNames.length,
-      );
+      const gameIndex = modulusThatHandlesNegatives(index + i - center, gameNames.length);
       const animatedDifference = animatedIndex - index;
       const gameName = gameNames[gameIndex];
-      const y =
-        canvasRect.height / 2 +
-        (i - center - animatedDifference) * (fontSize + spacing);
+      const y = canvasRect.height / 2 + (i - center - animatedDifference) * (fontSize + spacing);
 
-      const distFromCenter = Math.min(
-        1,
-        Math.abs(i - center - animatedDifference) *
-          (1 / numberToDrawAboveAndBelow),
-      );
+      const distFromCenter = Math.min(1, Math.abs(i - center - animatedDifference) * (1 / numberToDrawAboveAndBelow));
       ctx.globalAlpha = 1 - distFromCenter ** 0.5;
       ctx.fillText(gameName, canvasRect.width / 2, y);
       ctx.globalAlpha = 1;
@@ -74,7 +63,8 @@ export function updateAndDraw(
     return;
   }
 
-  games[game](canvas, ctx, dt);
+  const [_gameName, updateAndDraw] = game;
+  updateAndDraw(canvas, ctx, dt);
 }
 
 function modulusThatHandlesNegatives(n: number, m: number) {
@@ -85,10 +75,10 @@ export function returnToMenu() {
   game = null;
 }
 
-document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowDown") index++;
-  if (e.key === "ArrowUp") index--;
-  if (e.key === "Enter") {
-    game = gameNames[modulusThatHandlesNegatives(index, gameNames.length)];
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowDown') index++;
+  if (e.key === 'ArrowUp') index--;
+  if (e.key === 'Enter') {
+    game = games[modulusThatHandlesNegatives(index, games.length)];
   }
 });
