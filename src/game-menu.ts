4ca6f90f1e880e89b5ff0bb46b4266abcdc379c7
Austin Merrick <austinmerrick@onsclom.net>
2024-07-21 22:22:37 -0700

    Start player info screen

--- src/game-menu.ts
+++ src/game-menu.ts
@@ -21,9 +21,33 @@ const games = [
   { name: "stacker", logic: Stacker },
 ] as const;
 
-let game = null as null | (typeof games)[number];
-let index = 0;
-let animatedIndex = 0;
+const colors = [
+  "#ff0000",
+  "#00ff00",
+  "#0000ff",
+  "#ffff00",
+  "#ff00ff",
+  "#00ffff",
+];
+
+const charsToChooseFrom = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ".split("");
+let state = {
+  state: "controller" as "controller" | "game",
+
+  playerInfo: Array(4)
+    .fill(null)
+    .map((_, i) => ({
+      name: `P${i + 1}  `.split(""),
+      nameIndexEditing: 0,
+
+      color: colors[i],
+    })),
+
+  game: null as null | (typeof games)[number],
+  index: 0,
+  animatedIndex: 0,
+};
+console.log(state);
 
 export function updateAndDraw(
   canvas: HTMLCanvasElement,
@@ -32,94 +56,251 @@ export function updateAndDraw(
 ) {
   // UPDATE
   /////////////
-  if (!game) {
+  if (state.state === "controller") {
     const gamepads = navigator.getGamepads();
     gamepads.forEach((gamepad) => {
       if (!gamepad) return;
       if (
+        justPressed(Buttons.DPAD_LEFT, gamepad) ||
+        joystickJustMoved(Axes.LEFT_STICK_X, -1, gamepad)
+      ) {
+        state.playerInfo[gamepad.index].nameIndexEditing--;
+        playMenuChangeSound();
+      }
+      if (
+        justPressed(Buttons.DPAD_RIGHT, gamepad) ||
+        joystickJustMoved(Axes.LEFT_STICK_X, 1, gamepad)
+      ) {
+        state.playerInfo[gamepad.index].nameIndexEditing++;
+        playMenuChangeSound();
+      }
+
+      state.playerInfo[gamepad.index].nameIndexEditing = intuitiveModulus(
+        state.playerInfo[gamepad.index].nameIndexEditing,
+        state.playerInfo[gamepad.index].name.length,
+      );
+
+      const playerInfo = state.playerInfo[gamepad.index];
+      const currentLetterIndex = charsToChooseFrom.indexOf(
+        playerInfo.name[playerInfo.nameIndexEditing],
+      );
+      if (
         justPressed(Buttons.DPAD_DOWN, gamepad) ||
         joystickJustMoved(Axes.LEFT_STICK_Y, 1, gamepad)
       ) {
-        index++;
+        playerInfo.name[playerInfo.nameIndexEditing] =
+          charsToChooseFrom[
+            intuitiveModulus(currentLetterIndex + 1, charsToChooseFrom.length)
+          ];
         playMenuChangeSound();
       }
       if (
         justPressed(Buttons.DPAD_UP, gamepad) ||
         joystickJustMoved(Axes.LEFT_STICK_Y, -1, gamepad)
       ) {
-        index--;
+        playerInfo.name[playerInfo.nameIndexEditing] =
+          charsToChooseFrom[
+            intuitiveModulus(currentLetterIndex - 1, charsToChooseFrom.length)
+          ];
         playMenuChangeSound();
       }
-      if (
-        justPressed(Buttons.A, gamepad) ||
-        justPressed(Buttons.START, gamepad)
-      ) {
-        game = games[intuitiveModulus(index, games.length)];
-        saveGamepadState(); // reset just pressed for new game!
-        game.logic.reset();
+    });
+  } else {
+    const gamepads = navigator.getGamepads();
+    gamepads.forEach((gamepad) => {
+      if (!gamepad) return;
+      if (justPressed(Buttons.START, gamepad)) {
+        state.game = null;
+        console.log("SET GAME TO NULL");
       }
     });
+
+    if (!state.game) {
+      gamepads.forEach((gamepad) => {
+        if (!gamepad) return;
+        if (
+          justPressed(Buttons.DPAD_DOWN, gamepad) ||
+          joystickJustMoved(Axes.LEFT_STICK_Y, 1, gamepad)
+        ) {
+          state.index++;
+          playMenuChangeSound();
+        }
+        if (
+          justPressed(Buttons.DPAD_UP, gamepad) ||
+          joystickJustMoved(Axes.LEFT_STICK_Y, -1, gamepad)
+        ) {
+          state.index--;
+          playMenuChangeSound();
+        }
+        if (justPressed(Buttons.A, gamepad)) {
+          state.game = games[intuitiveModulus(state.index, games.length)];
+          saveGamepadState(); // reset just pressed for new game!
+          state.game.logic.reset();
+        }
+      });
+    } else {
+      state.game.logic.update(dt);
+    }
+    state.animatedIndex += (state.index - state.animatedIndex) * 0.015 * dt;
   }
-  animatedIndex += (index - animatedIndex) * 0.015 * dt;
 
   // DRAW
   /////////////
-  if (!game) {
-    // render menu
-    const fontSize = 30;
-    ctx.fillStyle = "white";
-    ctx.font = `${fontSize}px Arial`;
-    ctx.textAlign = "center";
-    const gameNames = games.map((game) => game.name);
-    const canvasRect = canvas.getBoundingClientRect();
-    const numberToDrawAboveAndBelow = 4;
-    const spacing = 20;
-    const totalToDraw = numberToDrawAboveAndBelow * 2 + 1;
-    const centerOffset = Math.floor(totalToDraw / 2);
-    ctx.filter = `blur(${Math.abs(index - animatedIndex) * 2}px)`;
-    {
-      const animatedIndexFloored = Math.floor(animatedIndex);
-      for (let i = 0; i < totalToDraw; i++) {
-        const gameIndex = intuitiveModulus(
-          animatedIndexFloored + i - centerOffset,
-          gameNames.length,
+  if (state.state === "controller") {
+    const gamepads = navigator.getGamepads();
+    const drawingArea = canvas.getBoundingClientRect();
+    const sectionWidth = drawingArea.width / gamepads.length;
+    ctx.strokeStyle = "white";
+    gamepads.forEach((gamepad, i) => {
+      const playerRect = {
+        x: sectionWidth * i,
+        y: 0,
+        width: sectionWidth,
+        height: drawingArea.height,
+      };
+      ctx.strokeRect(
+        playerRect.x,
+        playerRect.y,
+        playerRect.width,
+        playerRect.height,
+      );
+
+      if (!gamepad) {
+        // draw text (not connected)
+        ctx.fillStyle = "white";
+        ctx.font = "30px Arial";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(
+          "Not Connected",
+          playerRect.x + playerRect.width / 2,
+          playerRect.y + playerRect.height / 2,
         );
-        const animatedDecimal = animatedIndex - animatedIndexFloored;
-        const gameName = gameNames[gameIndex];
-        const y =
-          canvasRect.height / 2 +
-          (i - centerOffset - animatedDecimal) * (fontSize + spacing);
-        const distFromCenter = Math.min(
-          1,
-          Math.abs(i - centerOffset - animatedDecimal) *
-            (1 / numberToDrawAboveAndBelow),
+      } else {
+        // // draw name
+        // ctx.fillStyle = "white";
+        // ctx.font = "30px Arial";
+        // ctx.textAlign = "center";
+        // ctx.textBaseline = "middle";
+        // ctx.fillText(
+        //   state.playerInfo[i].name.join(""),
+        //   playerRect.x + playerRect.width / 2,
+        //   playerRect.y + playerRect.height / 2,
+        // );
+        ctx.save();
+        ctx.translate(playerRect.x, playerRect.y);
+
+        const gridSection = {
+          width: 50,
+          height: 50,
+        };
+        const grid = {
+          widthParts: 4,
+          heightParts: 3,
+        };
+        const gridRect = {
+          width: grid.widthParts * gridSection.width,
+          height: grid.heightParts * gridSection.height,
+        };
+
+        ctx.strokeStyle = "white";
+        ctx.translate(
+          playerRect.width / 2 - gridRect.width / 2,
+          playerRect.height / 2 - gridRect.height / 2,
         );
-        ctx.globalAlpha = 1 - distFromCenter ** 0.5;
-        ctx.fillText(gameName, canvasRect.width / 2, y);
+        ctx.strokeRect(0, 0, gridRect.width, gridRect.height);
+
+        const name = state.playerInfo[i].name;
+        name.forEach((letter, i) => {
+          ctx.fillStyle = state.playerInfo[gamepad.index].color;
+          ctx.font = "30px Arial";
+          ctx.textAlign = "center";
+          ctx.textBaseline = "middle";
+          const x = i % grid.widthParts;
+          const y = Math.floor(i / grid.widthParts);
+          ctx.fillText(
+            letter,
+            x * gridSection.width + gridSection.width / 2,
+            y * gridSection.height +
+              gridSection.height / 2 +
+              gridSection.height,
+          );
+        });
+
+        const indexEditing = state.playerInfo[i].nameIndexEditing;
+        // "^" "v"
+
+        ctx.fillText(
+          "↑",
+          indexEditing * gridSection.width + gridSection.width / 2,
+          gridSection.height / 2,
+        );
+        ctx.fillText(
+          "↓",
+          indexEditing * gridSection.width + gridSection.width / 2,
+          gridSection.height / 2 + gridSection.height * 2,
+        );
+
+        // Array(grid.heightParts)
+        //   .fill(null)
+        //   .forEach((_, y) => {
+        //     Array(grid.widthParts)
+        //       .fill(null)
+        //       .forEach((_, x) => {
+        //         ctx.strokeRect(
+        //           x * gridSection.width,
+        //           y * gridSection.height,
+        //           gridSection.width,
+        //           gridSection.height,
+        //         );
+        //       });
+        //   });
+
+        ctx.restore();
       }
-      ctx.globalAlpha = 1;
+    });
+  } else {
+    if (!state.game) {
+      // render menu
+      const fontSize = 30;
+      ctx.fillStyle = "white";
+      ctx.font = `${fontSize}px Arial`;
+      ctx.textAlign = "center";
+      const gameNames = games.map((game) => game.name);
+      const canvasRect = canvas.getBoundingClientRect();
+      const numberToDrawAboveAndBelow = 4;
+      const spacing = 20;
+      const totalToDraw = numberToDrawAboveAndBelow * 2 + 1;
+      const centerOffset = Math.floor(totalToDraw / 2);
+      ctx.filter = `blur(${Math.abs(state.index - state.animatedIndex) * 2}px)`;
+      {
+        const animatedIndexFloored = Math.floor(state.animatedIndex);
+        for (let i = 0; i < totalToDraw; i++) {
+          const gameIndex = intuitiveModulus(
+            animatedIndexFloored + i - centerOffset,
+            gameNames.length,
+          );
+          const animatedDecimal = state.animatedIndex - animatedIndexFloored;
+          const gameName = gameNames[gameIndex];
+          const y =
+            canvasRect.height / 2 +
+            (i - centerOffset - animatedDecimal) * (fontSize + spacing);
+          const distFromCenter = Math.min(
+            1,
+            Math.abs(i - centerOffset - animatedDecimal) *
+              (1 / numberToDrawAboveAndBelow),
+          );
+          ctx.globalAlpha = 1 - distFromCenter ** 0.5;
+          ctx.fillText(gameName, canvasRect.width / 2, y);
+        }
+        ctx.globalAlpha = 1;
+      }
+      return;
+    } else {
+      state.game.logic.draw(canvas, ctx);
     }
-    return;
   }
 
-  const gamepads = navigator.getGamepads();
-  gamepads.forEach((gamepad) => {
-    if (!gamepad) return;
-    if (justPressed(Buttons.START, gamepad)) {
-      // setPause(!paused);
-      // game
-      game = null;
-    }
-  });
-
-  if (!paused) {
-    game?.logic?.update(dt);
-  }
-  if (game !== null) {
-    ctx.save();
-    game.logic.draw(canvas, ctx);
-    ctx.restore();
-  }
   const drawRect = canvas.getBoundingClientRect();
   if (paused) {
     ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
@@ -138,5 +319,5 @@ function intuitiveModulus(n: number, m: number) {
 }
 
 export function returnToMenu() {
-  game = null;
+  state.game = null;
 }
